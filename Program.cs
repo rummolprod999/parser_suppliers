5022a10e08ed0c4900960d889f7ce4174aac4b6a
rummolprod999 <rummolprod999@gmail.com>
2017-03-27 12:21:00 +0300

    update

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace parser_suppliers
+{
+    internal class Program
+    {
+        private static string Db = "tenders_test";
+        private static string sfx = "";
+        private static DateTime localDate = DateTime.Now;
+
+        public static void Main(string[] args)
+        {
+
+            string _fileLog = String.Format("./log_suppliers/suppliers_{0}.log", localDate.ToString("dd_MM_yyyy"));
+            int period = 0;
+            int downCount = 10;
+            string archive = "";
+            while (downCount >=0)
+            {
+                --downCount;
+            }
+
+        }
+    }
+}
